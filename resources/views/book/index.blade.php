3a125888bea4d07b7c1bfb858d9ecb3b478340d1
Dennys5 <dennys.rocca07@gmail.com>
2023-11-28 13:46:41 +0100

    installation de breeze

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -2,12 +2,11 @@
 @section('title', 'Books')
 @section('content')
 
-
 <div class="bigCont">
     <h1>helloooooo</h1>
-    @foreach($book as $books => $details )
+    @foreach($book as $books => $details)
     <div class="container">
-        <a class="link" href="{{route('book.show', $details->id)}}">  <h2 class="title">{{$details->title}}</h2></a> 
+        <a class="link" href="{{route('book.show', $details->id)}}"><h2 class="title">{{$details->title}}</h2></a> 
         <a  href="{{route('book.edit', $details->id)}}">Modifier</a> 
         <p>{{$details->author}}</p> 
         <p>{{$details->year}}</p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>@yield('title')</title>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-</head>
-<body>
-    <header class="content">
-        <h1>@yield('title')</h1>
-        <nav>
-            <a href="{{route('book.index')}}"<i class="fa-solid fa-cat"></i>>List</a>
-            <a href="{{route('book.create')}}"<i class="fa-solid fa-cat"></i>>New fucking book</a>
-        </nav>
-        <br>
-    </header>
-    <main class="content">
-        @yield('content')
-    </main>
-    <footer class="content">
-        &copy; Le Fucking Bocal Academy 2085
-    </footer>
-</body>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="stylesheet" href="/style.css">
+        {{-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white dark:bg-gray-800 shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                        <a href="#">Bonjour</a>
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                @yield('content')
+            </main>
+        </div>
+    </body>
 </html>
