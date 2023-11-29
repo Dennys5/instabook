f7af61cb464cbbe951b59a2a3ec0455983e7d479
Zaïnab <zeinab0695@gmail.com>
2023-11-29 09:10:09 +0100

    Modèle Genre, clé étrangère, upload image

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         Book::factory(20)->create();
 
         $this->call([
-            BookSeeder::class
+            BookSeeder::class,
+            GenreSeeder::class
         ]);
     }
 }
--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -3,20 +3,24 @@
 @section('title', 'Détails d\'un Book')
 
 @section('content')
-    <h2>{{$book['title']}}
-        <form action="{{route('book.destroy', $book['id'])}}" method="post" class="tool">
-            @csrf
-            @method('delete')
-            <button type="submit">
-             Delete the motherfucker
-            </button>
-        </form>
-    </h2>
+    <h2>{{$book['title']}}  </h2>
     <ul>
         <li>Auteur : {{$book['author']}}</li>
         <li>Genre : {{$book['genre']}}</li>
         <li>Année : {{$book['year']}}</li>
         <li>Note : {{$book['note']}}</li>
+        <img src="{{asset("storage/images/".$book['image'])}}" alt="{{$book['title']}}">
     </ul>
+    Note livre : <input type='number' name='note' placeholder='Note' value='{{old('note')}}'><br>
+@if($errors->has('note'))
+    <p>{{$errors->first('note')}}</p>
+@endif
+    <form action="{{route('book.destroy', $book['id'])}}" method="post" class="tool">
+        @csrf
+        @method('delete')
+        <button type="submit">
+         Delete the motherfucker
+        </button>
+    </form>
 
 @endsection
